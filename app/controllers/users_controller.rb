0120f37841a8a30168325e49c6c5240833e1cbb2
kashi-iyo <snakeman143@gmail.com>
2020-03-23 19:18:36 +0900

    ユーザー一覧機能追加、デフォルト画像の設定
    機能追加のため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+
+  end
+
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      image_name: "default_img.png"
+    )
+    if @user.save
+      flash[:notice] = "登録が完了したお"
+      redirect_to("/users/index")
+    else
+      render("users/new")
+    end
+  end
 end
