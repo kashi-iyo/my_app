32950d5567729c34422434273611e67a85c4d913
kashi-iyo <snakeman143@gmail.com>
2020-03-25 22:49:56 +0900

    紐付け機能追加
    新機能追加のため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+    @posts = @user.posts
   end
 
   def edit
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,9 @@ class Post < ApplicationRecord
                       uniqueness: true
 
   validates :user_id, presence: true
-                        
+
   def user
     return User.find_by(id: self.user_id)
   end
+  
 end
